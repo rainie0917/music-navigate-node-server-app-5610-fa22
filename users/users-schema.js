efe8d77f40d31eeff6116533fa04f1ab71f0f409
Rainie Zhao <rainiezhao@Rainies-MacBook-Pro-2.local>
2022-11-30 17:00:57 -0800

    edit mongoose connection string

--- users/users-schema.js
+++ users/users-schema.js
@@ -3,10 +3,10 @@ import mongoose from "mongoose";
 const usersSchema = mongoose.Schema({
     username: {type: String, required: true, unique: true},
     password: {type: String, required: true},
+    role: {type: String, require:true, enum: ['ADMIN', 'FAN', 'SINGER'], default: 'FAN'},
     firstName: String,
     lastName: String,
     email: String,
-    role: {type: String, enum: ['ADMIN', 'CRITIC', 'FAN']},
 }, {collection: 'users'})
 
 export default usersSchema
