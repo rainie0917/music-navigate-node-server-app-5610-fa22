7d1e712a78ff46a467c56dec67c9f3d593bf1619
Xun Fang <xunfang@Xuns-MacBook-Air.local>
2022-12-14 13:20:07 -0800

    revised homepage for anonymous users

--- controllers/songs/songs-dao.js
+++ controllers/songs/songs-dao.js
@@ -1,7 +1,8 @@
 import songsModel from '../songs/songs-models.js';
 
+
 export const findSongs = () => songsModel.find();
-export const findSortedSongs = () => songsModel.find().sort("likes");
+export const findSortedSongs = () => songsModel.find().sort({"likes":-1});
 
 
 export const updateSong = (sid, song) => songsModel.updateOne({_id: sid}, {$set: song})
