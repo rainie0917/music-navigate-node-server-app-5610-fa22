e0c09d1727d9293a9c590566ac96a6e2207d2eb4
Xun Fang <xunfang@Xuns-MacBook-Air.local>
2022-12-14 21:16:15 -0800

    before meeting with Wenbo

--- controllers/songs/songs-schema.js
+++ controllers/songs/songs-schema.js
@@ -1,9 +1,21 @@
-import mongoose from 'mongoose';
-const schema = mongoose.Schema({
-  musicName: String,
-  playerName: String,
-  AlbumName: String,
-  likes: Number,
-  liked: Boolean,
-}, {collection: 'songs'});
-export default schema;
+// import mongoose from 'mongoose';
+// const schema = mongoose.Schema({
+//   musicName: String,
+//   playerName: String,
+//   AlbumName: String,
+//   likes: Number,
+//   liked: Boolean,
+// }, {collection: 'songs'});
+// export default schema;
+
+const songsSchema = new mongoose.Schema(
+    {
+      likes: {type: Number},
+      whoLiked: {type: [String]},
+      artist: {type: String},
+      realImg: {type: String},
+      mbid: {type: String},
+      name: {type: String},
+    },
+    {collection: 'songs'}
+)
