8001d082edda3ba4c5daf4e9a2589c556964fcb2
Xun Fang <xunfang@Xuns-MacBook-Air.local>
2022-12-14 23:47:26 -0800

    Homepage part done

--- controllers/songs/songs-schema.js
+++ controllers/songs/songs-schema.js
@@ -1,30 +1,13 @@
-<<<<<<< HEAD
 import mongoose from 'mongoose';
-=======
-// import mongoose from 'mongoose';
-// const schema = mongoose.Schema({
-//   musicName: String,
-//   playerName: String,
-//   AlbumName: String,
-//   likes: Number,
-//   liked: Boolean,
-// }, {collection: 'songs'});
-// export default schema;
-
->>>>>>> dev2
 const songsSchema = new mongoose.Schema(
     {
-      likes: {type: Number},
-      whoLiked: {type: [String]},
-      artist: {type: String},
-      realImg: {type: String},
-      mbid: {type: String},
-      name: {type: String},
+          likes: {type: Number},
+          whoLiked: {type: [String]},
+          artist: {type: String},
+          realImg: {type: String},
+          mbid: {type: String},
+          name: {type: String},
     },
     {collection: 'songs'}
-<<<<<<< HEAD
 )
 export default songsSchema;
-=======
-)
->>>>>>> dev2
