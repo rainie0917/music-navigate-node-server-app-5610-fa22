c1daa4ba86ec28462d6ac4f6bcaf45e23392bf8e
Wenbo Pan <wenbopan313@gmail.com>
2022-12-14 20:52:42 -0800

    like & unlike

--- controllers/songs/songs-schema.js
+++ controllers/songs/songs-schema.js
@@ -1,9 +1,13 @@
 import mongoose from 'mongoose';
-const schema = mongoose.Schema({
-  musicName: String,
-  playerName: String,
-  AlbumName: String,
-  likes: Number,
-  liked: Boolean,
-}, {collection: 'songs'});
-export default schema;
+const songsSchema = new mongoose.Schema(
+    {
+      likes: {type: Number},
+      whoLiked: {type: [String]},
+      artist: {type: String},
+      realImg: {type: String},
+      mbid: {type: String},
+      name: {type: String},
+    },
+    {collection: 'songs'}
+)
+export default songsSchema;
