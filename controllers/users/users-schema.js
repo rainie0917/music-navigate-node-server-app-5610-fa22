62ddf449afe73239f949727f9734a7082395e6c3
Wenbo Pan <wenbopan313@gmail.com>
2022-12-15 21:37:12 -0800

    add follow& unfollow

--- controllers/users/users-schema.js
+++ controllers/users/users-schema.js
@@ -8,7 +8,8 @@ const usersSchema = mongoose.Schema({
     firstName: {type: String, required: true},
     lastName: {type: String, required: true},
     email: {type: String, required: true},
-    likedSongs: {type: [String]}
+    likedSongs: {type: [String]},
+    following: {type: [String]}
 
 }, {collection: 'users'})
 
